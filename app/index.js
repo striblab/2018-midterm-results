cc23dbb968ea1f9e10ee6143b09518b910c934e8
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-10-17 14:09:17 -0500

    Create civix api class.

--- app/index.js
+++ app/index.js
@@ -9,42 +9,14 @@
 
 // Dependencies
 import utils from './shared/utils.js';
+import Civix from './shared/civix.js';
+import Content from '../templates/_index-content.svelte.html';
 
 // Mark page with note about development or staging
 utils.environmentNoting();
 
-// Adding dependencies
-// ---------------------------------
-// Import local ES6 or CommonJS modules like this:
-// import utilsFn from './shared/utils.js';
-//
-// Or import libraries installed with npm like this:
-// import module from 'module';
-
-// Adding Svelte templates in the client
-// ---------------------------------
-// We can bring in the same Svelte templates that we use
-// to render the HTML into the client for interactivity.  The key
-// part is that we need to have similar data.
-//
-// First, import the template.  This is the main one, and will
-// include any other templates used in the project.
-import Content from '../templates/_index-content.svelte.html';
-//
-// Get the data parts that are needed.  There are two ways to do this.
-// If you are using the buildData function to get data, then ?
-//
-// 1. For smaller datasets, just import them like other files.
-import content from '../content.json';
-//
-// 2. For larger data points, utilize window.fetch.
-// let content = await (await window.fetch('../assets/data/content.json')).json();
-//
-// Once you have your data, use it like a Svelte component:
-//
+// Svelte template hook-up
 const app = new Content({
   target: document.querySelector('.article-lcd-body-content'),
-  data: {
-    content
-  }
+  data: {}
 });
---
+++ app/shared/civix.js
@@ -0,0 +1,103 @@
+/**
+ * Class to access civix data
+ * https://github.com/striblab/civix/
+ */
+
+// Main civix class
+class Civix {
+  constructor(resource, options = {}) {
+    options.endpoint =
+      options.endpoint || '//static.startribune.com/elections/civix-test/v2/';
+    options.election = options.election || '2018-11-05';
+    options.jitter =
+      options.jitter === false || options.jitter === true
+        ? options.jitter
+        : true;
+    options.polling = 30 + (options.jitter ? Math.random() * 5 : 0);
+    options.autostart =
+      options.autostart === false || options.autostart === true
+        ? options.autostart
+        : false;
+    this.options = options;
+    this.resource = resource;
+
+    if (!this.resource) {
+      throw new Error('Resource not provided to Civix api class.');
+    }
+
+    // Make sure we know what the formatting of the URl is
+    this.options.endpoint = this.options.endpoint.replace(/\/+$/, '');
+    this.resource = `${this.resource
+      .replace(/^\/+/, '')
+      .replace(/.json$/i, '')}.json`;
+
+    // Initialize events
+    this.events = {};
+
+    // Allow to not automatically start
+    if (this.options.autostart) {
+      return this.poll();
+    }
+
+    return this;
+  }
+
+  // On
+  on(event, action) {
+    this.events[event] = this.events[event] || [];
+    this.events[event].push(action);
+    return this;
+  }
+
+  // Trigger
+  trigger(event, ...args) {
+    if (this.events[event] && this.events[event].length) {
+      this.events[event].forEach(action => {
+        action(...args.concat(this));
+      });
+    }
+  }
+
+  // Poll
+  poll() {
+    this.fetch();
+
+    // Don't make another interval
+    if (!this.intervalId) {
+      this.intervalId = window.setInterval(() => {
+        this.fetch();
+      }, this.options.polling * 1000);
+    }
+
+    return this;
+  }
+
+  // Stop
+  cancel() {
+    if (this.intervalId) {
+      window.clearInterval(this.intervalId);
+    }
+
+    return this;
+  }
+
+  // Do api fetch
+  fetch() {
+    return window
+      .fetch(
+        `${this.options.endpoint}/${this.options.election}/${this.resource}`
+      )
+      .then(response => {
+        return response.json();
+      })
+      .then(parsed => {
+        this.trigger('update', parsed);
+      })
+      .catch(e => {
+        console.error(e);
+        this.trigger('error', e);
+      });
+  }
+}
+
+export default Civix;
