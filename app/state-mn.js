a77aac4da197e4d0cb849c24cf326e970743b816
CJ Sinner <cj.sinner@startribune.com>
2018-10-19 10:33:16 -0500

    trying to get this new page to load

---
+++ app/state-mn.js
@@ -0,0 +1,22 @@
+/**
+ * Main JS file for project.
+ */
+
+// Define globals that are added through the js.globals in
+// the config.json file, here, mostly so linting won't get triggered
+// and its a good queue of what is available:
+// /* global _ */
+
+// Dependencies
+import utils from './shared/utils.js';
+import Civix from './shared/civix.js';
+import Content from '../templates/_state-mn-content.svelte.html';
+
+// Mark page with note about development or staging
+utils.environmentNoting();
+
+// Svelte template hook-up
+const app = new Content({
+  target: document.querySelector('.article-lcd-body-content'),
+  data: {}
+});
